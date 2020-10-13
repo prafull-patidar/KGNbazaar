81456a27248694e05fd7d618a965be86d684f8e4
prafull-patidar <61003627+prafull-patidar@users.noreply.github.com>
2020-10-13 23:18:12 +0530

    Update settings.py
    smtp credentials

--- KGNbazaar/settings.py
+++ KGNbazaar/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'prafull0099@gmail.com'
-EMAIL_HOST_PASSWORD = 'gymfreakme@7878'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 PASSWORD_RESET_TIMEOUT_DAYS = 1
