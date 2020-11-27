3325474e92a6012f60fca8de95e34d406de5a03e
prafull-patidar <61003627+prafull-patidar@users.noreply.github.com>
2020-11-27 13:07:52 +0530

    Update settings.py

--- KGNbazaar/settings.py
+++ KGNbazaar/settings.py
@@ -162,7 +162,7 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prafull0099@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
